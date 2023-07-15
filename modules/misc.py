37dd1583ed8b9573f1f6872c02c8ba94349c99a0
alekm <alekmurray@gmail.com>
2023-07-15 13:56:30 -0400

    Add files via upload
    Ability to add to server via link.
    Added /hamlive and modified verbiage of existing commands to clarify

--- modules/misc.py
+++ modules/misc.py
@@ -51,7 +51,7 @@ class MiscCog(commands.Cog):
             .generate(
                 title="Help",
                 description=hb_about + self.calc_uptime(),
-                footer='hambot 1.1.0 by N4OG\n'
+                footer='hambot 1.2 by N4OG\n'
                        '\tbased on HamTheMan by thisguyistotallyben'
             ), ephemeral=True
         )
@@ -62,6 +62,22 @@ class MiscCog(commands.Cog):
         embed.set_image(url='https://blog.hamstudy.org/wp-content/uploads/2013/10/hamstudy_blue.png')
         await ctx.respond(embed=embed)
 
+    @slash_command(name="testing", description="License Testing Information")
+    async def testing(self, ctx):
+        await ctx.respond(embed=self.embed_service
+                .generate(
+                    title="Taking your ham license test with HRCC",
+                    description=hb_testing
+                    ), ephemeral=False
+                )
+    @slash_command(name="hamlive", description="Ham.Live Net Information")
+    async def hamlive(self,ctx):
+            await ctx.respond(embed=self.embed_service
+                .generate(
+                    title="Ham.Live",
+                    description=hamlive_text
+                    ), ephemeral=True
+                )
 
     def calc_uptime(self):
         up = str(timedelta(seconds=(time.time() - self.bot.start_time)))
@@ -116,12 +132,16 @@ help_message = ('**Core commands**\n'
                 '\t`/call [callsign]`: Callsign information (Sources: HamQTH'
                 ', callook.info)\n'
                 '\t`/dx [prefix]`: DXCC information about a call prefix\n'
+                '\t`/study`: Studying for your ham license test\n'
+                '\t`/testing`: How to take your ham license test\n'
+                '\t`/hamlive`: Information on using Ham.Live for the HRCC HF Net\n'
                 '\n\t`/about`: About the bot\n'
                 '\t`/uptime`: Bot uptime\n')
 
 
 hb_about = ('**Author**\n'
-             '\tAlek, N4OG\n'
+             '\tAlek, N4OG\n\n'
+             '\tAdd me to your server! https://discordapp.com/oauth2/authorize?client_id=947361185878147082&scope=bot&permissions=67488832\n\n'
              '\n**Tools**\n'
              '\tPython 3.10\n'
              '\tPy-Cord 2.0.0-beta.7\n'
@@ -132,35 +152,69 @@ hb_about = ('**Author**\n'
              '\tMaximum Usable Frequency (MUF) data from prop.kc2g.com\n'
              '\n**Uptime**\n\t')
 
-study_text = ("A good way to study is with the ham.study application. You can install the "
-              "application on a phone or tablet, or you can use it on-line. So, if you don't want to "
-              "pay the $4 for the application, you can just access it through a browser from any "
-              "device, even if you're not connected to the Internet. If you access hamstudy with a "
-              "browser, it's always free, but you do need to Register with your email address for it "
-              "to keep track of your progress.\n"
-              'In either case, you should create an account by "Registering" on hamstudy.org. Do '
-              'not use Google or Facebook - register with an email address. This creates a free '
-              "account that keeps track of your progress.\n"
-              "Once you've Registered for your account, do this:\n"
-              "Login to ham.study using your username and password.\n"
-              "Choose the Technician (2018 - 2022) exam by clicking on Technician (2018 - 2022):\n"
-              "Click on Study Mode:\n"
-              "Use the drop-down option in the top bar to change from All Questions to just T1:\n"
-              "Click on T1.\n"
-              "Now go through each question in T1, until you've Seen 100% of the questions, and "
-              "your Aptitude is 85% or more.\n"
-              "Only then go to the next Sub element (T2).\n"
-              "Continue doing this with each sub element.\n"
-              "Do not skip sub elements.\n"
-              "Do not take practice exams until you've Seen 100% of each sub element and your "
-              "Aptitude is 85% or more in each sub element.\n"
-              "The bar graph on the right will display your Seen and Aptitude.\n"
-              "If you have any questions about how to use hamstudy, or questions about the "
-              "questions and answers, just reply to this email. There's an explanation of the answer "
-              "when you're in Study Mode. Just click on I don't know. The reason for I don't know "
-              "instead of guessing is the app is designed to give you questions more frequently if "
-              "you select I don't know instead of getting it wrong.\n\n"
+study_text = ("Ham Radio Crash Course Technician License Prep (2022-2026): https://www.youtube.com/watch?v=DdzQS10JnHU&list=PL1KAjn5rGhixvvb_jMZFWmbP97-t9Kyxk&index=1 \n\n"
+        "A good way to study is with the https://ham.study application. You can install the application on a phone or tablet, or you can use it on-line. So, if you don't want to pay the $4 for the application, you can just access it through a browser from any device, even if you're not connected to the Internet. If you access hamstudy with a browser, it's always free, but you do need to Register with your email address for it to keep track of your progress.\n\n"
+'In either case, you should create an account by "Registering" on hamstudy.org. Do not use Google or Facebook - register with an email address. This creates a free account that keeps track of your progress.\n\n '
+"Once you've Registered for your account:\n\n"
+"   • Login to ham.study using your username and password.\n"
+"   • Choose the Technician exam by clicking on Technician (Starting Jul 1, 2022):\n"
+"   • Click on Study Mode:\n"
+"   • Use the drop-down option in the top bar to change from All Questions to just T1\n"
+"   • Click on T1.\n"
+"   • Go through each question in T1, until you've Seen 100% of the questions.\n"
+"   • Go to the next Sub element (T2) when your aptitude is at 85% or more.\n"
+"   • Continue doing this with each sub element.\n"
+"   • Do not skip sub elements.\n"
+"   • Do not take practice exams until you've Seen 100% of each sub element and your Aptitude is 85% or more in each sub element.\n"
+"   • The bar graph on the right will display your Seen and Aptitude.\n\n"
+"There's an explanation of the answer when you're in Study Mode. Just click on I don't know. The reason for I don't know instead of guessing is the app is designed to give you questions more frequently if you select I don't know instead of getting it wrong.\n\n"
+"Once you are done studying for Technician, you can do the same for General and Extra. Substitute the appropriate element you are studying. All credit for this method goes to Norm, K6YXH.")
 
-              "Once you are done studying for Technician you can do the same for General and "
-              "Extra when ready. You would just substitute the appropriate element you are "
-              "studying. All credit for this method goes to Norm K6YXH\n")
+hb_testing = ("For more information about getting your ham license with the HRCC VE team, check this link: "
+                "\n\nhttps://hrcc.wiki/en/home/VETesting")
+
+hamlive_text = ("Ham.Live allows for a more interactive experience, by providing real-time signal reporting "
+                "between net attendees and the ability to visually watch the net progress.\n\n"
+
+                "To get setup with Ham.Live, please click on the dedicated HRCC HF Net Link\n" 
+                "https://www.ham.live/views/livenet/637836da8531e399631ee065\n\n"
+
+                "If you’re not logged in, you will be prompted to either login or set up an account. Account "
+                "setup is easy. Either login via Google (preferred) or via an “email link”. Once "
+                "authenticated, you will be prompted to accept terms and conditions and provide your "
+                "callsign and location. Once enrolled, go back the aforementioned link:\n\n"
+
+                "https://www.ham.live/views/livenet/637836da8531e399631ee065\n\n"
+
+                "This is the net “waiting area” for the HRCC HF Net. If the net is running, you will "
+                "immediately be placed into the live running net.\n\n"
+
+                "If the net is not running, you’ll see “HRCC HF Net - Waiting for Net Start”. Be sure to "
+                "click the purple star next to “HRCC HF Net” (so it’s solid purple) this will ensure that "
+                "you’re following the net ongoing.\n\n"
+
+                "If you arrive early, the “waiting area” will change to the live/running net screen "
+                "automatically once Net Control starts the net.\n\n"
+
+                "During the net you’re encouraged to provide signal reports to other attendees (assuming "
+                "Net Control has the feature enabled). Adjacent to each attendee’s name you’ll see an input "
+                "text box which looks like [  RS  ]. You can give anyone (other than yourself) a signal "
+                "report, as long as they’re checked-in to the net. Reports are averaged in real-time. So "
+                "if you receive a 59 and a 57 your RS field will indicate 58. You can give another station "
+                "multiple signal reports, but doing so only replaces the prior report *you gave them (in "
+                "other words you can’t skew the average by giving someone a 59 10 times in a row, for example. "
+                "Each station gets one ‘vote’ to influence another station's overall report, so to speak). At "
+                "the end of the net the finalized signal reports will be included in Net Control’s report "
+                "and can be sent out to the group discord, if desired.\n\n"
+
+                "The system allows for additional interactivity, for instance attendees can raise their hands "
+                "(NCS can lower your hand). Stations can be highlighted by NCS. You can see another member’s "
+                "location simply by hovering your mouse over their name.\n\n"
+
+                "If you login via Google, your profile picture is whatever you happen to use with Google. "
+                "If you login via the “Email Link” option, you can change your profile pic via the following "
+                "URL:\n\n"
+
+                "https://en.gravatar.com\n\n"
+
+                "If you encounter any technical issues with the system feel free to contact support@ham.live")
